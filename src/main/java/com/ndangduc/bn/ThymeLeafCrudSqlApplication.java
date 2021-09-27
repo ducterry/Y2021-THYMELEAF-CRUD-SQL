d9f810bfcd6fe0812b3ccc091cf900075e5cb61c
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-09-27 13:54:36 +0700

    Initial Commit

---
+++ src/main/java/com/ndangduc/bn/ThymeLeafCrudSqlApplication.java
@@ -0,0 +1,13 @@
+package com.ndangduc.bn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafCrudSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeLeafCrudSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndangduc/bn/ThymeLeafCrudSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndangduc.bn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafCrudSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
