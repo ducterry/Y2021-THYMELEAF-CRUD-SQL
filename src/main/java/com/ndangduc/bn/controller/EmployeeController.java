176d76e53e863ac63e2a7b6413cb1e9f09c7a143
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-10-05 21:01:38 +0700

    2021.10.05: Delete Employees

--- src/main/java/com/ndangduc/bn/controller/EmployeeController.java
+++ src/main/java/com/ndangduc/bn/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.ndangduc.bn.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class EmployeeController {
@@ -38,8 +35,14 @@ public class EmployeeController {
     }
 
     @GetMapping("/showFormForUpdate/{id}")
-    public String showNewEmployeeForm(@PathVariable(value = "id") long id, Model model) {
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
         model.addAttribute("employee", this.employeeService.getEmployeeById(id));
         return "update_employee";
+    }
+
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") long id, Model model) {
+       this.employeeService.deleteEmployeeById(id);
+        return "redirect:/";
     }
 }
